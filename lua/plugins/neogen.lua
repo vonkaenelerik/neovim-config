679fce0926fe74588f3dd025e86607ebd06f6392
Erik Vonkaenel <evonkaenel@host-22.home>
2025-06-01 20:49:40 -0600

    docstring stuff, added better snippet commands, better diagnostics

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,6 +1,27 @@
 return{
 	"danymat/neogen",
-	config = true,
-	-- Uncomment next line if you want to follow only stable versions
-	-- version = "*"
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	config = function()
+		require('neogen').setup({
+			enabled = true,
+			languages = {
+				cs = {
+					template = {
+						annotation_convention = "xmldoc"
+					}
+				}
+			},
+			snippet_engine = "luasnip",
+		})
+	end,
+	keys = {
+		{
+			"<leader>nf",
+			function()
+				require('neogen').generate()
+			end,
+			desc = "Generate documentation"
+		}
+	},
 }
+
