8b01bdaee85e1c7424c479c15f6b41e56fc225f0
Erik Vonkaenel <evonkaenel@host-22.home>
2025-07-02 07:49:51 -0600

    qol plugins

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,22 +1,20 @@
 return {
-	'stevearc/conform.nvim',
+	"stevearc/conform.nvim",
 	opts = {},
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				go = { "gofmt" },
 				r = { "air" },
 				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 				sql = { "sqruff" },
 				cs = { "csharpier" },
 			},
-			-- format_on_save = {
-			-- 	-- These options will be passed to conform.format()
-			-- 	timeout_ms = 500,
-			-- 	lsp_format = "fallback",
-			-- },
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
 		})
-	end
+	end,
 }
-
